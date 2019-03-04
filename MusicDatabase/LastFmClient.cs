17259046cf46c0bdc2bd7a1fea2c62d1e5898a3a
PsychoHacks <jamie.v732@gmail.com>
2019-03-04 14:58:53 +0000

    Updated project with current files from last week

---
+++ MusicDatabase/LastFmClient.cs
@@ -0,0 +1,25 @@
+﻿using IF.Lastfm.Core.Api;
+using IF.Lastfm.Core.Api.Helpers;
+using IF.Lastfm.Core.Objects;
+using System.Threading.Tasks;
+
+namespace MusicDatabase
+{
+    class LastFmClient
+    {
+        public async Task<PageResponse<LastAlbum>> Get(string search)
+        {
+            var client = new LastfmClient("2bc24890053b55f27e991f944c448586", "a0bba8ca5ee7f334ea353e95422c7356");
+            var response = await client.Album.SearchAsync(search);
+            return response;
+        }
+
+        public async Task<LastAlbum> GetAlbumInfo(string artist, string album)
+        {
+            var client = new LastfmClient("2bc24890053b55f27e991f944c448586", "a0bba8ca5ee7f334ea353e95422c7356");
+            var response = await client.Album.GetInfoAsync(artist, album);
+            LastAlbum albumInfo = response.Content;
+            return albumInfo;
+        }
+    }
+}
---
+++ MusicDatabase/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Threading.Tasks;
+using IF.Lastfm.Core.Objects;
+using IF.Lastfm.Core.Api.Helpers;
+
+namespace MusicDatabase
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PageResponse<LastAlbum> response;
+        LastFmClient client;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnSearch_ClickAsync(object sender, RoutedEventArgs e)
+        {
+            var search = txbSearch.Text;
+            client = new LastFmClient();
+            response = await client.Get(search);
+
+            foreach (var item in response.Content)
+            {
+                cbxResults.Items.Add(item.ArtistName +"-"+ item.Name);
+            }
+        }
+
+        private async void CbxResults_SelectionChangedAsync(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            string selected = cbxResults.SelectedValue.ToString();
+            string[] info = selected.Split('-');
+            string artist = info[0];
+            string album = info[1];
+
+            client = new LastFmClient();
+            var albumInfo = await client.GetAlbumInfo(artist, album);
+            txtTest.Text = $"{albumInfo.ArtistName} - {albumInfo.Name}\n";
+            imgAlbum.Source = new BitmapImage(new Uri(albumInfo.Images.Large.ToString()));
+
+            var tracks = albumInfo.Tracks.ToList();
+
+
+            foreach (var item in tracks)
+            {
+                txtTest.Text += $"{item.Rank}: {item.Name}\n";
+            }
+            if (tracks.Count() == 0)
+            {
+                txtTest.Text = "No track info available!\n";
+            }
+        }
+    }
+}
